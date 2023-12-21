b134f1c0f88aa7396c1dc07865f2d89979180f4d
Edison <edisonstuart@gmail.com>
2023-12-21 14:19:35 -0500

    WIP: Updated user_client documentation. Added README. added pytest action.

--- orodha_user_client/client.py
+++ orodha_user_client/client.py
@@ -4,8 +4,8 @@ which is used to connect to and communicate with the Orodha User Service on beha
 of other Orodha services.
 """
 import os
-import requests
 from http import HTTPStatus
+import requests
 from orodha_user_client.exceptions import UrlNotFound, RequestError
 from orohda_keycloak import OrodhaCredentials, OrodhakeycloakClient
 
@@ -14,13 +14,52 @@ BULK_USER_URL = "get-bulk-users"
 
 class OrodhaUserClient:
     """
-    The main class for the orodha_user_client package. Allows services to make requests
-    to the Orodha User Service progromatically without having to make raw API calls.
+    The main class for the orodha_user_client package. Allows services
+    to make requests to the Orodha User Service programmatically without
+    having to make raw API calls.
     """
     def __init__(self, credentials: OrodhaCredentials):
         self.credentials = credentials
         self.keycloak_client = OrodhakeycloakClient(credentials_object=self.credentials)
         self.base_url = self._get_base_url()
+
+
+    def bulk_get(self, request_args: dict):
+        """
+        Function that calls the user service get-bulk-users route on
+        behalf of other services in the Orodha namespace.
+
+        Args:
+            request_args(dict): A dictionary of request arguments to be
+                packaged and sent to the Orodha user service.
+
+        Returns:
+            response(dict): A dictionary containing our new token info.
+        """
+        target_user = request_args.get("target_user")
+        if not target_user:
+            target_user = self.credentials.client_id
+
+        token_data = self.keycloak_client.exchange_token(target_user=target_user)
+
+        body = {
+            "pageSize": request_args.get("pageSize"),
+            "pageNum": request_args.get("pageNum"),
+            "targets": request_args.get("targets")
+        }
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization" : f"Bearer {token_data.get('access_token')}"
+        }
+        return_value = self._make_raw_request(
+            BULK_USER_URL,
+            "post",
+            body=body,
+            headers=headers
+        )
+
+        return return_value
+
 
     def _get_base_url(self):
         base_url = os.environ.get("ORODHA_USER_SERVICE_BASE_URL")
@@ -53,41 +92,4 @@ class OrodhaUserClient:
             return_value = response.json()
         except ValueError:
             return_value = response.content
-        return return_value
-
-
-    def bulk_get(self, request_args: dict):
-        """
-        Function that calls the user service get-bulk-users route on
-        behalf of other services in the Orodha namespace.
-
-        Args:
-            request_args(dict): A dictionary of request arguments to be packaged and sent
-                to the Orodha user service.
-
-        Returns:
-            response(dict): A dictionary containing our new token info.
-        """
-        target_user = request_args.get("target_user")
-        if not target_user:
-            target_user = self.credentials.client_id
-
-        token_data = self.keycloak_client.exchange_token(target_user=target_user)
-
-        body = {
-            "pageSize": request_args.get("pageSize"),
-            "pageNum": request_args.get("pageNum"),
-            "targets": request_args.get("targets")
-        }
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization" : f"Bearer {token_data.get('access_token')}"
-        }
-        return_value = self._make_raw_request(
-            BULK_USER_URL,
-            "post",
-            body=body,
-            headers=headers
-        )
-
         return return_value
